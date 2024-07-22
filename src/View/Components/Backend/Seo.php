3a3dfb6af5163f1d6011f37ec93bfca12f6346ba
Henry <asdcxz179@gmail.com>
2024-07-22 13:31:31 +0800

    fix

--- src/View/Components/Backend/Seo.php
+++ src/View/Components/Backend/Seo.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Oukuyun\Admin\View\Components\Backend;
@@ -28,34 +27,3 @@ class Seo extends Component
         return view("admin::components.backend.seo");
     }
 }
-=======
-<?php
-
-namespace Oukuyun\Admin\View\Components\Backend;
-
-use Illuminate\View\Component;
-
-class Seo extends Component
-{
-    public $fields;
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->fields = config('seo.fields');
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
-    public function render()
-    {
-        return view("admin::components.backend.seo");
-    }
-}
->>>>>>> seo2
